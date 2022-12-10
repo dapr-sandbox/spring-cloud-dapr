f90fa0e5943723128be2e5b458662616b46186fe
MregXN <mregxn@gmail.com>
2022-12-10 11:25:53 +0800

    add DaprGrpcServiceTest
    Signed-off-by: MregXN <mregxn@gmail.com>

--- spring-cloud-stream-dapr/spring-cloud-stream-binder-dapr/src/test/java/io/dapr/spring/cloud/stream/binder/DaprGrpcServiceTest.java
+++ spring-cloud-stream-dapr/spring-cloud-stream-binder-dapr/src/test/java/io/dapr/spring/cloud/stream/binder/DaprGrpcServiceTest.java
@@ -17,7 +17,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.Assert;
 import org.mockito.ArgumentCaptor;
 
@@ -25,9 +28,27 @@ import io.dapr.serializer.DaprObjectSerializer;
 import io.dapr.serializer.DefaultObjectSerializer;
 import io.dapr.spring.cloud.stream.binder.messaging.DaprMessageConverter;
 import io.dapr.v1.DaprAppCallbackProtos;
+import io.dapr.v1.DaprAppCallbackProtos.TopicSubscription;
 import io.grpc.stub.StreamObserver;
+import com.google.protobuf.Empty;
 
-public class DaprMessageProducerTest {
+public class DaprGrpcServiceTest {
+    private String pubsubName;
+    private String topic;
+    private DaprObjectSerializer objectSerializer;
+    private DaprMessageConverter daprMessageConverter;
+    private DaprGrpcService daprGrpcServic;
+    private DaprMessageProducer producer;
+
+    @BeforeEach
+    public void beforeEach() {
+        pubsubName = "pubsubName";
+        topic = "topic";
+        objectSerializer = new DefaultObjectSerializer();
+        daprMessageConverter = new DaprMessageConverter(objectSerializer);
+        daprGrpcServic = new DaprGrpcService();
+        producer = new DaprMessageProducer(daprGrpcServic, daprMessageConverter, pubsubName, topic);
+    }
 
     /**
      * The purpose of this test is to show if the response when GRPC event is called
@@ -35,14 +56,7 @@ public class DaprMessageProducerTest {
      * It can indicate subscribing message from Dapr sidecar is successful.
      */
     @Test
-    public void testSubscribe() {
-        String pubsubName = "pubsubName";
-        String topic = "topic";
-        DaprObjectSerializer objectSerializer = new DefaultObjectSerializer();
-        DaprMessageConverter daprMessageConverter = new DaprMessageConverter(objectSerializer);
-        DaprGrpcService daprGrpcServic = new DaprGrpcService();
-
-        DaprMessageProducer producer = new DaprMessageProducer(daprGrpcServic, daprMessageConverter, pubsubName, topic);
+    public void testOnTopicEvent() {
         DaprAppCallbackProtos.TopicEventRequest request = DaprAppCallbackProtos.TopicEventRequest.newBuilder().build();
         StreamObserver<DaprAppCallbackProtos.TopicEventResponse> responseObserver = mock(StreamObserver.class);
         ArgumentCaptor<DaprAppCallbackProtos.TopicEventResponse> captor = ArgumentCaptor
@@ -54,6 +68,30 @@ public class DaprMessageProducerTest {
         DaprAppCallbackProtos.TopicEventResponse response = captor.getValue();
         Assert.assertEquals(response.getStatus(),
                 DaprAppCallbackProtos.TopicEventResponse.TopicEventResponseStatus.SUCCESS);
+        verify(responseObserver, times(1)).onCompleted();
+    }
+
+    /**
+     * This test is to show if the subscription information can be sent successfully
+     * via GRPC.
+     */
+    @Test
+    public void testListTopicSubscriptionst() {
+        Empty request = Empty.newBuilder().build();
+        StreamObserver<DaprAppCallbackProtos.ListTopicSubscriptionsResponse> responseObserver = mock(
+                StreamObserver.class);
+        ArgumentCaptor<DaprAppCallbackProtos.ListTopicSubscriptionsResponse> captor = ArgumentCaptor
+                .forClass(DaprAppCallbackProtos.ListTopicSubscriptionsResponse.class);
+
+        daprGrpcServic.listTopicSubscriptions(request, responseObserver);
+
+        verify(responseObserver, times(1)).onNext(captor.capture());
+        DaprAppCallbackProtos.ListTopicSubscriptionsResponse response = captor.getValue();
+        List<TopicSubscription> list = response.getSubscriptionsList();
+
+        Assert.assertEquals(list.size(), 1);
+        Assert.assertEquals(list.get(0).getPubsubName(), "pubsubName");
+        Assert.assertEquals(list.get(0).getTopic(), "topic");
         verify(responseObserver, times(1)).onCompleted();
     }
 }
